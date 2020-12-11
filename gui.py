86110aa14401a26dde0e8e78435ed8d9412e9f95
Vincent <vincent.sahler@gmail.com>
2020-12-11 20:33:16 +0100

    External gui.py file for easier reading

---
+++ gui.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+
+class GUI:
+    def __init__(self, cfg, sender):
+        self.cfg = cfg
+        self.sender = sender
+        hosts, scenes, presets = cfg.fetchClasses()
+        presets_names = cfg.fetchPresetsNames()
+        self.root = tk.Tk()
+
+        self.root.geometry("500x500")
+        self.root.title("NDI-Control")
+
+        self.l = tk.LabelFrame(self.root, text="Selection du preset", padx=20, pady=20)
+        self.l.pack(fill="both", expand="yes")
+
+        self.preset_selection = tk.StringVar(self.root)
+        self.preset_selection.set(presets_names[0])
+
+        self.opt = tk.OptionMenu(self.l, self.preset_selection, *presets_names)
+        self.opt.config(width=90, font=('Helvetica', 12))
+        self.opt.pack(side="top")
+
+        self.l2 = tk.LabelFrame(self.root, text="Commandes", padx=20, pady=20)
+        self.l2.pack(fill="both", expand="yes")
+
+        self.button = tk.Button(self.l2, text="Envoyer", command=self.sendcallback)
+        self.button.pack()
+
+        self.preset_selection.trace("w", self.callback)
+
+        self.root.mainloop()
+
+    def sendcallback(self):
+        print("Sending commands")
+        self.sender.applyPresetFromName(self.preset_selection.get())
+
+    def callback(self, *args):
+        print("The selected item is {}".format(self.preset_selection.get()))
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import tkinter as tk
 import sys
 
 import api
 import config
 import midi
+import gui
 
 flags = []
 
@@ -39,48 +39,16 @@ if "help" in flags:
     print(" --no-midi      : Deactivate midi input")
     exit(0)
 
+# ---------- CONFIG READ ----------
 cfg = config.ConfigReader()
-hosts, scenes, presets = cfg.fetchClasses()
-presets_names = cfg.fetchPresetsNames()
 
+# ---------- SENDER ----------
 sender = api.Sender(cfg)
 
+# ---------- MIDI ----------
 midi_inst = midi.MidiHandler(cfg, flags, sender)
 flags = midi_inst.register()
-print("Registered")
+# print("Registered")
 
-root = tk.Tk()
-
-root.geometry("500x500")
-root.title("NDI-Control")
-
-l = tk.LabelFrame(root, text="Selection du preset", padx=20, pady=20)
-l.pack(fill="both", expand="yes")
-
-preset_selection = tk.StringVar(root)
-preset_selection.set(presets_names[0])
-
-opt = tk.OptionMenu(l, preset_selection, *presets_names)
-opt.config(width=90, font=('Helvetica', 12))
-opt.pack(side="top")
-
-l2 = tk.LabelFrame(root, text="Commandes", padx=20, pady=20)
-l2.pack(fill="both", expand="yes")
-
-
-def sendcallback():
-    print("Sending commands")
-    sender.applyPresetFromName(preset_selection.get())
-
-
-button = tk.Button(l2, text="Envoyer", command=sendcallback)
-button.pack()
-
-
-def callback(*args):
-    print("The selected item is {}".format(preset_selection.get()))
-
-
-preset_selection.trace("w", callback)
-
-root.mainloop()
+# ---------- GUI ----------
+gui.GUI(cfg, sender)  # [WARN] This is a blocking function
